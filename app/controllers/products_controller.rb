cf9471f6222800bb28f134dffd4c2ac4d8ea20fe
Scott Weinstein <sweinstein27@gmail.com>
2018-11-30 19:39:27 +0000

    Done.

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -23,12 +23,16 @@ class ProductsController < ApplicationController
   end
 
   def show
-    @product = Product.find(params[:id])
-  end
+     @product = Product.find(params[:id])
+     respond_to do |format|
+       format.html { render :show }
+       format.json { render json: @product.to_json(only: [:id, :name, :description, :price, :inventory])}
+     end
+ end
 
   def data
     product = Product.find(params[:id])
-    render json: ProductSerializer.serialize(product)
+    render json: product.to_json(only: [:id, :name, :description, :inventory, :price])
   end
 
   private
